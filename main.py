49b1340f4dc0a0773648c03fdda246fc4eedf6b5
ElzaScerbinska <124616741+ElzaScerbinska@users.noreply.github.com>
2023-02-18 14:22:49 +0000

    -

--- main.py
+++ main.py
@@ -24,8 +24,7 @@ def find_mismatch(text):
             opening_brackets_stack.pop()
     if opening_brackets_stack:
         return opening_brackets_stack[-1].position
-    else:
-        return "Success"
+    return "Success"
 
 def main():
     text = input()
