f2fb02d36a4c5ee95d31f42e657e2b52cffb228e
ElzaScerbinska <124616741+ElzaScerbinska@users.noreply.github.com>
2023-02-18 14:03:13 +0000

    -

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def find_mismatch(text):
 
 def main():
     text = input()
-    if "I\r\n" in text:
+    if "I" in text:
         text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
