c83357032991a200c60f43d65c1e1c71aff9c081
ElzaScerbinska <124616741+ElzaScerbinska@users.noreply.github.com>
2023-02-18 14:52:34 +0000

    -

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def find_mismatch(text):
 def main():
     text = input()
     if "I" in text:
-        text = input() 
+        text = input()
         mismatch = find_mismatch(text)
         print(mismatch)
 
