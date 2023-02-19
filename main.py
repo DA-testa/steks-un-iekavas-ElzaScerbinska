d23e36fa03e474829983187b198d526d074e0190
ElzaScerbinska <124616741+ElzaScerbinska@users.noreply.github.com>
2023-02-19 13:08:40 +0000

    -

--- main.py
+++ main.py
@@ -18,16 +18,20 @@ def find_mismatch(text):
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+            if (not opening_brackets_stack or
+                not are_matching(opening_brackets_stack[-1].char, next)):
                 return i+1
 
             opening_brackets_stack.pop()
-    if opening_brackets_stack:
+    if not opening_brackets_stack:
+        return "Success"
+    else:
         return opening_brackets_stack[-1].position
-    return "Success"
+    
 
 def main():
     text = input()
+    mismatch = find_mismatch(text)
     if "I" in text:
         text = input()
         mismatch = find_mismatch(text)
